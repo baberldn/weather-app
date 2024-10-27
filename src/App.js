43413848848f9ca3602143a0648013db247b83b1
baberldn <berilbadan1903@gmail.com>
2024-10-27 22:36:02 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import './styles.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-import { FaSun, FaMoon } from 'react-icons/fa';
 const App = () => {
     const [city, setCity] = useState("");
     const [weatherData, setWeatherData] = useState({});
-    const [theme, setTheme] = useState("day-theme");
+    const cityInputRef = useRef(null);
     const apiKey = "0930e6c90c94c842169fa276feca882a";
+
     useEffect(() => {
-        const currentHour = new Date().getHours();
-        if (currentHour >= 6 && currentHour < 18) {
-            setTheme("day-theme");
-        } else {
-            setTheme("night-theme");
+        if (cityInputRef.current) {
+            cityInputRef.current.focus();
         }
-    }, []);
- 
+    }, [city]);
+
     useEffect(() => {
         const getData = async () => {
             if (city === "") return;
- 
+
             try {
                 const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
                 if (!response.ok) {
@@ -31,27 +28,33 @@ const App = () => {
                 console.error("Error:", error);
             }
         };
- 
+
         getData();
     }, [city]);
- 
+
     return (
-        <div className={`weather-app ${theme}`}>
+        <div className="weather-app">
             <h1>Weather App</h1>
             <input
+                ref={cityInputRef}
                 type="text"
                 placeholder="Enter city name"
                 value={city}
                 onChange={(e) => setCity(e.target.value)}
             />
             <div className="icon">
-                {theme === "day-theme" ? <FaSun size={40} /> : <FaMoon size={40} />}
+                {city && weatherData.weather && (
+                    <img
+                        src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+                        alt="Weather Icon"
+                    />
+                )}
             </div>
             {city && weatherData.name && (
-                <div>
+                <div className="weather-info">
                     <h2>Weather in {weatherData.name}</h2>
                     <p>Temperature: {(weatherData.main.temp - 273.15).toFixed(2)}°C</p>
-                    <p>Weather: {weatherData.weather && weatherData.weather[0].description}</p>
+                    <p>Weather: {weatherData.weather[0].description}</p>
                     <p>Humidity: {weatherData.main.humidity}%</p>
                     <p>Wind: {weatherData.wind.speed} m/s</p>
                 </div>
@@ -60,4 +63,4 @@ const App = () => {
     );
 };
 
-export default App
+export default App;
